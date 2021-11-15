510861dc7efc771b1f7a4a59b09775146d23de21
grey <margaretbratt@gmail.com>
2021-11-15 11:24:12 -0800

    Add files via upload

---
+++ site/snippets/footer.php
@@ -0,0 +1,11 @@
+
+<footer>
+                <div class="footName">
+                    <?= $site->title() ?>
+                </div>
+                <div class="footInsta">
+                    <a href="http://instagram.com/<?= $site->instagram() ?>" target="_blank">
+                        <img class="instag" src="assets/images/instabw.png" alt="Link to Grey's Instagram" />
+                    </a>
+                </div>
+</footer>
